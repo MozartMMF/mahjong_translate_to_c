1d95a6a6983859546e4f519042528b0fb6519569
CaioMTRodrigues <105224573+CaioMTRodrigues@users.noreply.github.com>
2023-03-19 21:35:38 -0300

    Add files via upload

---
+++ estilo.h
@@ -0,0 +1,11 @@
+#ifndef STYLE_H_INCLUDED
+#define STYLE_H_INCLUDED
+
+void printBars(int heitgh);
+void printMenuTitle();
+void printDificultyTittle();
+void printMenuStyle();
+void printDificultyStyle();
+void printGameStyle();
+
+#endif
