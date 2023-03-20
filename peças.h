6487f84dd19942866212d58709cf61f574452c3a
CaioMTRodrigues <105224573+CaioMTRodrigues@users.noreply.github.com>
2023-03-19 21:34:28 -0300

    Add files via upload

---
+++ peças.h
@@ -0,0 +1,10 @@
+#ifndef PIECES_H_INCLUDED
+#define PIECES_H_INCLUDED
+
+void printPiece(int x, int y, int symbol);
+void randomizePiecesEasy();
+void randomizePiecesMedium();
+void randomizePiecesHard();
+
+#endif
+
